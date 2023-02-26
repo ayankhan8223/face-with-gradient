ab682212eb5190ced66b40cd936d6d110689beb0
ayankhan8223 <ayankhan8223@gmail.com>
2023-02-26 21:12:42 +0530

    added_ar

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,7 @@
-import "./style.css";
 import ReactDOM from "react-dom/client";
-import { Canvas } from "@react-three/fiber";
-import Experience from "./Experience.jsx";
-import { Suspense } from "react";
-import { Bounds } from "@react-three/drei";
-import { Interactive, XR, ARButton, Controllers } from "@react-three/xr";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
-root.render(
-  <>
-    <ARButton />
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [5, 0, 5],
-      }}
-    >
-      <XR referenceSpace="local">
-        <color attach="background" args={["#000000"]} />
-      </XR>
-      <Experience />
-    </Canvas>
-  </>
-);
+root.render(<App />);
